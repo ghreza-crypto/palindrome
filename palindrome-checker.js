c3f378d8a8ca5f663190ab0fe0e18ffd71659165
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-02-03 22:30:50 +0430

    palindrom solution

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,3 +1,9 @@
 module.exports = function (str) {
-  // write your code here
+  let str = str.replace(/\W/g, "");
+  let str2=str.split("").reverse().join();
+  if(str===str2){
+    return true;
+  }else{
+    return false;
+  }
 }
