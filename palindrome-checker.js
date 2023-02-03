7daf71e8fc3ffc3990f4d886f444329f3128df10
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-02-03 22:45:14 +0430

    fixed error

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,9 +1,10 @@
 module.exports = function (str) {
-   str = str.replace(/\W/g, "");
-  let str2=str.split("").reverse().join();
-  if(str===str2){
-    return true;
-  }else{
-    return false;
-  }
+  let replaced = str.replace( /([^\w]+|\s+)/g, '').toLowerCase();
+  let noSpace = replaced.replace('_', '')
+  let split = noSpace.split("");
+  let backwards = split.reverse();
+  let joined = backwards.join('');
+  if (joined === noSpace){
+    return true
+  } else { return false}
 }
